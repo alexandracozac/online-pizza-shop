71faad2ddaa24949c0e4087fa236ccae0e513462
Ale Cozac <ale_ioana98@yahoo.com>
2019-12-07 15:28:24 +0200

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/onlinepizzashop/OnlinePizzaShopApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlinepizzashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePizzaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePizzaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/onlinepizzashop/OnlinePizzaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlinepizzashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePizzaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
